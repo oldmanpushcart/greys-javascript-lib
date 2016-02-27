aa6d55f88dc239f187f92654ad3554e4d1c4f50d
oldmanpushcart <oldmanpushcart@gmail.com>
2016-02-28 01:09:21 +0800

    Update sql-monitor.js

--- script/sql-monitor.js
+++ script/sql-monitor.js
@@ -1,89 +1,72 @@
 __greys_require({
     paths: {
-        tui: 'https://raw.githubusercontent.com/oldmanpushcart/greys-anatomy/master/scripts/text-formatting-module.js',
-        stats: 'https://raw.githubusercontent.com/oldmanpushcart/greys-anatomy/master/scripts/stream-statistics-module.js',
-        lang: 'https://raw.githubusercontent.com/oldmanpushcart/greys-anatomy/master/scripts/common-lang-module.js',
+        tui: 'https://raw.githubusercontent.com/oldmanpushcart/greys-javascript-lib/master/script/lib/text-formatting-module.js',
+        stats: 'https://raw.githubusercontent.com/oldmanpushcart/greys-javascript-lib/master/script/lib/stream-statistics-module.js',
+        lang: 'https://raw.githubusercontent.com/oldmanpushcart/greys-javascript-lib/master/script/lib/common-lang-module.js',
+        scheduler: 'https://raw.githubusercontent.com/oldmanpushcart/greys-javascript-lib/master/script/lib/scheduler-module.js',
     }
 })
 
 /**
  * 模版
  */
-__greys_require(['greys', 'lang', 'tui', 'stats'], function (greys, lang, tui, stats) {
+__greys_require(['greys', 'lang', 'tui', 'stats', 'scheduler'], function (greys, lang, tui, stats, scheduler) {
 
     // 监控数据(K(id):V(stats,sql))
     var monitor = {};
+    var timer;
+    var lock = new lang.lock();
 
     // Statement解析器
     var parsers = [
 
-        // mysql-jdbc-connector-parser
+        // mysql-jdbc-connector-parser on execute()
         {
             test: function (advice) {
                 return /^com\.mysql\.jdbc\.PreparedStatement$/.test(advice.clazz.name)
-                    && /^(execute|executeQuery|executeUpdate|executeBatch)$/.test(advice.method.name)
-                    && lang.array.isEmpty(advice.params)
+                    && /^(execute|executeQuery|executeUpdate|executeBatch|executeLargeBatch|executeLargeUpdate|)$/.test(advice.method.name)
             },
 
             parse: function (advice) {
-                return advice.target.getNonRewrittenSql();
+                if (lang.array.isEmpty(advice.params)) {
+                    return advice.target.getNonRewrittenSql();
+                }
+
+                else if (params[0] && lang.string.isNotBlank("" + params[0])) {
+                    return "" + advice.params[0];
+                }
+
+                else {
+                    return "UNKNOW-SQL";
+                }
             }
-        }
+        },
+
     ];
 
-    var limit = 100;
+    function _create(output) {
+        timer = new scheduler();
+        timer.setInterval(function () {
 
-    function getParser(advice) {
-        var it = lang.array.iterator(parsers);
-        while (it.hasNext()) {
-            var parse = it.next();
-            if (parse.test(advice)) {
-                return parse;
+            var _monitor;
+            lock.lock();
+            try {
+                _monitor = monitor;
+                monitor = {};
+            } finally {
+                lock.unlock();
             }
-        }
-    }
-
-    function finish(output, advice, context) {
-
-        var parse = getParser(advice);
-        if (!parse) {
-            return;
-        }
-
-        var sql = parse.parse(advice);
-        if (!sql) {
-            return;
-        }
-
-        var stat = monitor[sql];
-        if (!stat) {
-            stat = monitor[sql] = stats.create([
-                stats.SUM,
-                stats.SUM,
-                stats.SUM,
-                stats.AVG,
-                stats.MIN,
-                stats.MAX,
-            ]);
-        }
-
-        stat.stats(
-            1,
-            advice.isReturning ? 1 : 0,
-            advice.isThrowing ? 1 : 0,
-            context.getCost(),
-            context.getCost(),
-            context.getCost()
-        );
-
-        if (limit-- <= 0) {
-            limit = 100;
 
             var table = new tui.table();
             table.config({
                 borders: ['top', 'bottom', 'left', 'right', 'vertical', 'horizontal'],
                 padding: 1,
                 columns: [
+                    {
+                        width: 20,
+                        vertical: 'middle',
+                        horizontal: 'left'
+                    },
                     {
                         width: 80,
                         vertical: 'middle',
@@ -123,13 +106,14 @@ __greys_require(['greys', 'lang', 'tui', 'stats'], function (greys, lang, tui, s
             });
 
             // add title
-            table.row('SQL', 'TOTAL', 'SUCCESS', 'FAIL', 'RT', 'RT-MIN', 'RT-MAX');
+            table.row('TIMESTAMP', 'SQL', 'TOTAL', 'SUCCESS', 'FAIL', 'RT', 'RT-MIN', 'RT-MAX');
+            var timestamp = lang.date.format(new Date(), 'yyyy-MM-dd hh:mm:ss');
 
-            for (var sql in monitor) {
-                var stat = monitor[sql];
+            for (var sql in _monitor) {
+                var stat = _monitor[sql];
                 var report = stat.stats();
-
                 table.row(
+                    "" + timestamp,
                     "" + sql,
                     "" + report[0],
                     "" + report[1],
@@ -138,15 +122,70 @@ __greys_require(['greys', 'lang', 'tui', 'stats'], function (greys, lang, tui, s
                     "" + (report[4].toFixed(2)),
                     "" + (report[5].toFixed(2))
                 );
-                monitor[sql] = null;
             }
 
             output.println(table.rendering());
+
+        }, 1000 * 60);
+    }
+
+    function _destroy() {
+        timer.shutdown();
+    }
+
+    function getParser(advice) {
+        var it = lang.array.iterator(parsers);
+        while (it.hasNext()) {
+            var parse = it.next();
+            if (parse.test(advice)) {
+                return parse;
+            }
+        }
+    }
+
+    function finish(output, advice, context) {
+
+        var parse = getParser(advice);
+        if (!parse) {
+            return;
+        }
+
+        var sql = parse.parse(advice);
+        if (!sql) {
+            return;
+        }
+
+        lock.lock();
+        try {
+            var stat = monitor[sql];
+            if (!stat) {
+                stat = monitor[sql] = stats.create([
+                    stats.SUM,
+                    stats.SUM,
+                    stats.SUM,
+                    stats.AVG,
+                    stats.MIN,
+                    stats.MAX,
+                ]);
+            }
+
+            stat.stats(
+                1,
+                advice.isReturning ? 1 : 0,
+                advice.isThrowing ? 1 : 0,
+                context.getCost(),
+                context.getCost(),
+                context.getCost()
+            );
+        } finally {
+            lock.unlock();
         }
 
     }
 
     greys.watching({
+        create: _create,
+        destroy: _destroy,
         returning: finish,
         throwing: finish,
     });

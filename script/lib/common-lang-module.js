6459e274d3efa24d1ea0c4c61d2b2b5661368b71
oldmanpushcart <oldmanpushcart@gmail.com>
2016-02-28 01:08:24 +0800

    Update common-lang-module.js

--- script/lib/common-lang-module.js
+++ script/lib/common-lang-module.js
@@ -475,15 +475,55 @@ define('__common_lang_java', ['__common_lang_string', '__common_lang_array'], fu
 
 })
 
+define('__common_lang_date', [], function () {
+    return {
+
+        /**
+         * 日期格式化
+         * 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符
+         * 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
+         * Example
+         * yyyy-MM-dd hh:mm:ss.S => 2006-07-02 08:09:04.423
+         *      yyyy-M-d h:m:s.S => 2006-7-2 8:9:4.18
+         * @param date
+         * @param format
+         * @returns {*}
+         */
+        format: function (date, format) {
+            var o = {
+                "M+": date.getMonth() + 1,                      //月份
+                "d+": date.getDate(),                           //日
+                "h+": date.getHours(),                          //小时
+                "m+": date.getMinutes(),                        //分
+                "s+": date.getSeconds(),                        //秒
+                "q+": Math.floor((date.getMonth() + 3) / 3),    //季度
+                "S": date.getMilliseconds()                     //毫秒
+            };
+            if (/(y+)/.test(format)) format = format.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+            for (var k in o) {
+                if (new RegExp("(" + k + ")").test(format)) {
+                    format = format.replace(RegExp.$1, (RegExp.$1.length == 1)
+                        ? (o[k])
+                        : (("00" + o[k]).substr(("" + o[k]).length)));
+                }
+            }
+
+            return format;
+        }
+
+    }
+})
+
 require(
-    ['__common_lang_string', '__common_lang_array', '__common_lang_text', '__common_lang_java', '__common_lang_lock'],
-    function (string, array, text, java, lock) {
+    ['__common_lang_string', '__common_lang_array', '__common_lang_text', '__common_lang_java', '__common_lang_lock', '__common_lang_date'],
+    function (string, array, text, java, lock, date) {
         module.exports = {
             array: array,
             string: string,
             text: text,
             java: java,
             lock: lock,
+            date: date,
         }
     })
 
